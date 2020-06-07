82e95bb580c95d98de912c787e8ea1d388b03c7c
Aditya Gupta <divyashgupta2005@gmail.com>
2020-06-07 16:47:11 +0530

    0.0.1

--- MathMaster/urls.py
+++ MathMaster/urls.py
@@ -19,5 +19,5 @@ from MathMaster import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),
+    path('home/', views.index),
 ]
--- MathMaster/views.py
+++ MathMaster/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home(request):
+def index(request):
     html = render(request, "index.html")
     return HttpResponse(html)
