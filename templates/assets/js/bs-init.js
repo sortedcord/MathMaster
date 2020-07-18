d0591c0f5fe718731c0c1b8c98dd18046199829d
Sortedcord <psilvei1@mail.ccsf.edu>
2020-07-18 10:52:52 +0530

    Cleaned Repo

--- templates/assets/js/bs-init.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	AOS.init({ disable: 'mobile' });
-
-	$('[data-bs-chart]').each(function(index, elem) {
-		this.chart = new Chart($(elem), $(elem).data('bs-chart'));
-	});
-
-});
